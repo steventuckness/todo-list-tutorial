1d1de0eda584e16ba7d76c149ec717cd7529e032
Steven Tuckness <stuckne1@gmail.com>
2019-07-26 06:11:19 -0500

    todo-list-tutorial - completed step 13

---
+++ src/app/list-manager/list-manager.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { TodoItem } from '../interfaces/todo-item';
+
+@Component({
+  selector: 'app-list-manager',
+  templateUrl: './list-manager.component.html',
+  styleUrls: ['./list-manager.component.css']
+})
+export class ListManagerComponent implements OnInit {
+
+  constructor() { }
+
+  todoList: TodoItem[] = [
+    {title: 'install NodeJS'},
+    {title: 'install Angular CLI'},
+    {title: 'create new app'},
+    {title: 'serve app'},
+    {title: 'develop app'},
+    {title: 'deploy app'},
+  ];
+
+  ngOnInit() {
+  }
+
+  addItem(title: string) {
+    this.todoList.push({title});
+  }
+
+}
