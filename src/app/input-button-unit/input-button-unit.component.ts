7dc2443f47720a0aa5c77b6fc24b947e2c2bc85a
Steven Tuckness <stuckne1@gmail.com>
2019-07-25 22:37:53 -0500

    todo-list-tutorial - completed step 5

--- src/app/input-button-unit/input-button-unit.component.ts
+++ src/app/input-button-unit/input-button-unit.component.ts
@@ -6,11 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./input-button-unit.component.css']
 })
 export class InputButtonUnitComponent implements OnInit {
-  title = 'Hello World';
+  title: string;
 
   constructor() { }
 
   ngOnInit() {
+    this.changeTitle('My First Angular App');
+  }
+
+  changeTitle(newTitle: string) {
+    this.title = newTitle;
   }
 
 }
