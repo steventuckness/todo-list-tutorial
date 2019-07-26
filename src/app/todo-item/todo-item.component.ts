a31cdfd497d54af7ea1939cd643e7be48d7503c4
Steven Tuckness <stuckne1@gmail.com>
2019-07-26 07:23:55 -0500

    todo-list-tutorial - completed step 19

--- src/app/todo-item/todo-item.component.ts
+++ src/app/todo-item/todo-item.component.ts
@@ -8,12 +8,19 @@ import { TodoItem } from '../interfaces/todo-item';
 })
 export class TodoItemComponent implements OnInit {
   @Input() item: TodoItem;
-
   @Output() remove: EventEmitter<TodoItem> = new EventEmitter();
+  @Output() update: EventEmitter<any> = new EventEmitter();
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  completeItem() {
+    this.update.emit({
+      item: this.item,
+      changes: {completed: !this.item.completed}
+    });
   }
 
   removeItem() {
